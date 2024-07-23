e9f6294af852b998bb69ea02c9971322d95db5ce
SaraValleGt <saravalle412@gmail.com>
2024-07-23 17:25:54 -0600

    Primer commit con el proyecto de Booking System API

---
+++ src/test/java/com/example/bookingsystemapi/BookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
